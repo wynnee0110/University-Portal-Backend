2ce5206459eecfb6992caca28476188c888903ae
Wayne <wynnee0110@gmail.com>
2026-03-05 02:43:38 +0800

    upd backed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 export default defineConfig({
-    schema: "./src/db/schema/*.ts",
+    schema: "./src/db/schema/index.js",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
